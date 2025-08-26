6cd7529c279369e2198df5fd4dbe78c92c847762
Rafaelpaka <rafael24un@gmail.com>
2025-08-26 17:26:18 -0300

    Mudanças, esqueci de enviar o coisa anterior,
    quando chegar na facul coloco o de verdade

---
+++ av1/Cadastro.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace av1
+{
+
+    public class Cadastro
+    {
+        public const string CaminhoArquivo = "biblioteca.json";
+        public static async Task<List<Jogo>> CarregarJogosAsinc()
+        {
+            if (File.Exists(CaminhoArquivo))
+            {
+                var JsonString = await File.ReadAllTextAsync(CaminhoArquivo);
+                return JsonSerializer.Deserialize<List<Jogo>>(JsonString) ?? new List<Jogo>();
+
+
+            }
+            return new List<Jogo>();
+
+        }
+
+        public static async Task SalvarJogosAsync(List<Jogo> Jogos)
+        {
+            var opcao = new JsonSerializerOptions { WriteIndented = true };
+            var JsonString = JsonSerializer.Serialize(Jogos, opcao);
+            await File.WriteAllTextAsync(CaminhoArquivo, JsonString);
+
+
+        }
+
+        public async Task CadastrarNovoJogoAsync()
+        {
+            Console.WriteLine("--- Cadastrar Jogo ---");
+
+            var JogosExistentes = await CarregarJogosAsync();
+
+            Console.WriteLine("Digite o nome do jogo: ");
+
+        }
+    }
+}
+
+            
+      
---
+++ av1/Jogo.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace av1
+{
+    public class Jogo
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Genero { get; set; }
+
+        public int Estado { get; set; }
+
+    }
+
+
+}
