03de18e562913a1f734944c050dbdcedc4f25891
Rafaelpaka <rafael24un@gmail.com>
2025-09-22 21:14:56 -0300

    correção emprestado anteriormente

---
+++ ConsoleApp1/Jogo.cs
@@ -0,0 +1,19 @@
+﻿namespace TrabalhoV1
+{
+    public class Jogo
+    {
+        public int Id { get; set; }
+        public string Nome { get;  set; }
+        public string Genero { get; set; }
+        public int Ano { get;  set; }
+        public bool Estado { get; set; } = true;
+        public string EmprestadoHa { get; set; } = "ninguem";
+
+        public DateTime? DataEmprestimo { get; set; }
+        public DateTime? DataDevolucao { get; set; }
+
+        public string FormaPagamento { get; set; }
+
+        public string EmprestadoAnteriormente { get; set; }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,86 @@
+﻿
+using TrabalhoV1;
+using TrabalhoV1.modelo;
+
+namespace program 
+{
+
+
+
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string opcao;
+            while (true)
+            {
+                
+
+                Console.WriteLine("\n=== LUDOTECA .NET ===");
+                Console.WriteLine("1 Cadastrar jogo");
+                Console.WriteLine("2 Excluir Jogo");
+                Console.WriteLine("3 Cadastrar membro");
+                Console.WriteLine("4 Excluir membro");
+                Console.WriteLine("5 Listar jogos");
+                Console.WriteLine("6 Emprestar jogo");
+                Console.WriteLine("7 Devolver jogo");
+                Console.WriteLine("8 Gerar relatório");
+                Console.WriteLine("0 Sair");
+                Console.Write("opção: ");
+                opcao = Console.ReadLine();
+
+                Emprestar emprestimo = new Emprestar();
+                ListaJogos Lista = new ListaJogos();
+                CadastroJogo cadastro = new CadastroJogo();
+                CadastroMembros cadastromem = new CadastroMembros();
+           
+                    switch (opcao)
+                {
+                    case "1":
+                        cadastro.Cadastrar(); // [AV1-4-Cadastrar]
+                        break;
+                    case "2":
+                        cadastro.Excluir(); // [AV1-4-Excluir]
+                        break;
+                    case "3":
+                        cadastromem.CadastroMembr(); // [AV1-4-CadastroMembro]
+                        break;
+                    case "4":
+                        cadastromem.ExcluirCad(); // [AV1-4-ExcluirMembro]
+                        break;
+                    case "5":
+                        Lista.Listar(); // [AV1-4-Listar]
+                        break;
+                    case "6":
+                        emprestimo.EmprestarJogo(); // [AV1-4-Emprestar]
+                        break;
+                    case "7":
+                        emprestimo.DevolverJogo(); // [AV1-4-Devolver]
+                        break;
+                    case "8":
+                        GerarRelatorio relatorio = new GerarRelatorio(); // [AV1-4-Relatorio]
+                        relatorio.RelatorioEmprestimos();
+                        break;
+                    default:
+                        Console.WriteLine("Opção indisponive, tente novamente");
+                        break;
+                    case "0":
+                        return; // [AV1-4-Sair]
+
+                    
+                }
+
+                Console.WriteLine("pressione Enter para continuar");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
+
+
+
+
+
+
